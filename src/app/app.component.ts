b74325b0083e341c026a089a113c27a71716db6f
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-05-04 01:06:23 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "./user.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  userActivated = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.activatedEmitter.subscribe((didActivate) => {
+      this.userActivated = didActivate;
+    });
+  }
+}
